7f6d130d2f7606025e2d85afcda29c503d56daa8
Mini <mershamini@gmail.com>
2026-02-21 18:19:34 +0530

    Add files via upload

---
+++ port scanner.py
@@ -0,0 +1,20 @@
+import socket
+
+target = input("Enter target IP: ")
+start_port = int(input("Enter start port: "))
+end_port = int(input("Enter end port: "))
+
+print(f"\nScanning {target}...\n")
+
+for port in range(start_port, end_port + 1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} is OPEN")
+    else:
+        print(f"Port {port} is closed")
+
+    s.close()
